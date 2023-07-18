d63876e63414ae7fd162f4a63df2582c852095a6
Nikita Butov <butov3084@gmail.com>
2023-07-18 13:28:00 +0500

    add capitalize_words

--- str_func.py
+++ str_func.py
@@ -1,3 +1,12 @@
 def capitalize_string(input_string):
     # Приводим строку к верхнему регистру
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+    """
+    переводит первые буквы в верхний регистр
+    """
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
